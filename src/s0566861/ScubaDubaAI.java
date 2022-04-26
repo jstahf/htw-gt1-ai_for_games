24d82c1d0a2dbec672cfecb5602f60280dd2ed26
Jannes <jannes.luca@hotmail.com>
2022-04-26 04:15:53 +0200

    final version for tournament. added comments

--- src/s0566861/ScubaDubaAI.java
+++ src/s0566861/ScubaDubaAI.java
@@ -15,28 +15,35 @@ import java.util.Arrays;
 
 public class ScubaDubaAI extends AI {
 
-    private Point2D target;
+    private Point2D target; // current target
+    private Point2D nextPearl; // next pearl targeted
 
-    ArrayList<Point> pearls = new ArrayList<>(Arrays.asList(info.getScene().getPearl()));
-    private Point2D nextPearl;
+    ArrayList<Point> pearls; // holds all remaining pearls
 
     private int score = 0;
 
-    private Point2D playerPos = new Point2D.Double(info.getX(), info.getY());
-    private boolean stuck = false;
-    private float stuckLimit = 0;
-    private boolean clockwise = false;
+    private Point2D playerPos; // last player position
 
-    private Point virtualPearl;
-    private boolean virtualPearlUsed = false;
+    private boolean stuck = false; // hasnt moved / is stuck
+    private float stuckLimit = 0; // counter for stuckness measurement
+    private boolean clockwise = false; // how to turn around obstacles
+
+    private Point virtualPearl; // virtual pearl for special cases
+    private boolean virtualPearlUsed; // virtualPearl already used?
 
 
 
     public ScubaDubaAI(Info info) {
         super(info);
 
-        virtualPearl = new Point (1580, 420);
+        pearls  = new ArrayList<>(Arrays.asList(info.getScene().getPearl()));
+
+        playerPos = new Point2D.Double(info.getX(), info.getY());
+
+        virtualPearl = new Point (1580, 450); // specific checkpoint for this seed, since pathfinding was bugged for last pearl
+        virtualPearlUsed = false;
         pearls.add(virtualPearl);
+
         enlistForTournament(566861);
     }
 
@@ -47,72 +54,75 @@ public class ScubaDubaAI extends AI {
 
     @Override
     public Color getPrimaryColor() {
-        return Color.black;
+        return Color.BLACK;
     }
 
     @Override
     public Color getSecondaryColor() {
-        return Color.MAGENTA;
+        return Color.CYAN;
     }
 
     @Override
     public PlayerAction update() {
 
 
-        if(playerPos.distance(new Point2D.Double(info.getX(), info.getY())) < 0.5 && stuckLimit<150) {
-            stuckLimit++;
+        if(playerPos.distance(new Point2D.Double(info.getX(), info.getY())) < 0.5 && stuckLimit<150) { // if diver hasn't moved much
+            stuckLimit++; // count towards stuck
         } else {
-            stuckLimit -= 1;
-            if(stuckLimit<0) stuckLimit = 0;
+            stuckLimit -= 1; // calm down, everything ok
+            if(stuckLimit<0) stuckLimit = 0; // no negatives
         }
 
 
 
-        if(stuckLimit>50) {
-            stuck = true;
-        } else {
-            stuck = false;
+       stuck =  (stuckLimit>50) ? true : false; // if too few moves for too long -> stuck
+
+
+        playerPos = new Point2D.Double(info.getX(), info.getY()); // save player position
+
+        if(playerPos.distance(new Point2D.Double(1580,450)) < 10){ // check if virtual pearl is in reach
+            pearls.remove(virtualPearl); // remove
+            virtualPearlUsed = true; // virtual pearl has been used up
         }
 
-        playerPos = new Point2D.Double(info.getX(), info.getY());
-        if(playerPos.distance(new Point2D.Double(1580,420)) < 10){
-            pearls.remove(virtualPearl);
-            virtualPearlUsed = true;
-        }
+        if(score < info.getScore()) { // if scored
 
-        if(score < info.getScore()) {
             Point scoredPearl = pearls.get(0);
 
-            for(Point pearl : pearls) {
-                if(pearl.distance(new Point2D.Double(info.getX(), info.getY())) < scoredPearl.distance(new Point2D.Double(info.getX(), info.getY()))) {
-                    scoredPearl = pearl;
+            for(Point pearl : pearls) { // iterate all pearls
+                if(pearl.distance(new Point2D.Double(info.getX(), info.getY())) < scoredPearl.distance(new Point2D.Double(info.getX(), info.getY()))) { // if distance is smaller than previous pearl
+                    scoredPearl = pearl; // take this pearl
                 }
             }
 
-            pearls.remove(scoredPearl);
-            stuckLimit = 0;
-            score++;
+            pearls.remove(scoredPearl); // remove it
+            stuckLimit = 0; // reset stuck counter
+            score++; // increase score
         }
 
-        this.nextPearl = getNextPearl();
+        this.nextPearl = getNextPearl(); // get next target pearl
 
-        target = nextPearl;
+        target = nextPearl; // set pearl as target
 
-        clockwise = false;
+        clockwise = false; // default
 
 
-        if((getPearlDirection() < Math.PI/2 || (getPearlDirection() > Math.PI*1.8 ))) {
+        if((getPearlDirection() < Math.PI/2 || (getPearlDirection() > Math.PI*1.8 ))) { // for certain directions counter clockwise is better
             clockwise = true;
         }
         if(stuck) {
-            clockwise = !clockwise;
+            clockwise = !clockwise; // if stuck, try other direction
         }
 
-        target = avoidObstacles(target, clockwise);
+        target = avoidObstacles(target, clockwise); // check for obstacles. if there is one -> target a point around it
 
-        return new DivingAction(info.getMaxAcceleration(), getTargetDirection());
+        return new DivingAction(info.getMaxAcceleration(), getTargetDirection()); // dive
     }
 
+    /**
+     *
+     * @return returns current player direction as radians
+     */
     private float getTargetDirection() {
 
         double xVec = target.getX() - info.getX();
@@ -125,6 +135,10 @@ public class ScubaDubaAI extends AI {
         return (float) direct;
     }
 
+    /**
+     *
+     * @return returns direction to next pearl in radians
+     */
     private float getPearlDirection() {
         double xVec = nextPearl.getX() - info.getX();
         double yVec = nextPearl.getY() - info.getY();
@@ -136,29 +150,36 @@ public class ScubaDubaAI extends AI {
         return (float) direct;
     }
 
+    /**
+     *
+     * @param target current target
+     * @param clockwise winding order
+     * @return returns an target free of obstacles
+     */
     private Point2D avoidObstacles(Point2D target, boolean clockwise) {
 
-        Line2D lineToTarget = new Line2D.Double(info.getX(), info.getY(), target.getX(), target.getY());
+        Line2D lineToTarget = new Line2D.Double(info.getX(), info.getY(), target.getX(), target.getY()); // line to target
 
-        Path2D[] obstacles = info.getScene().getObstacles();
+        Path2D[] obstacles = info.getScene().getObstacles(); // get all obstacles from scene
 
 
-
-        for(Path2D obstacle: obstacles) {
+        for(Path2D obstacle: obstacles) { // for every obstacle in obstacles
 
             Point lastPoint = null;
-            for(PathIterator pi = obstacle.getPathIterator(null); !pi.isDone(); pi.next()) {
 
+            for(PathIterator pi = obstacle.getPathIterator(null); !pi.isDone(); pi.next()) { // for every segment in obstacle
+
+                // get coordinates
                 double[] coordinates = new double[6];
                 pi.currentSegment(coordinates);
 
-                if(lastPoint == null) {
-                    lastPoint = new Point((int) coordinates[0], (int) coordinates[1]);
+                if(lastPoint == null) { // if first iteration
+                    lastPoint = new Point((int) coordinates[0], (int) coordinates[1]); // prevent out of bounds
                 }
 
-                Line2D segment = new Line2D.Double(lastPoint.getX(), lastPoint. getY(), coordinates[0],coordinates[1]);
+                Line2D segment = new Line2D.Double(lastPoint.getX(), lastPoint. getY(), coordinates[0],coordinates[1]); // create line from segment
 
-                if(segment.intersectsLine(lineToTarget) && coordinates[0] != 0 && coordinates[1]!= 0) {
+                if(segment.intersectsLine(lineToTarget) && coordinates[0] != 0 && coordinates[1]!= 0) { // if line to target intersects segment AND not last iteration (preventing out of bounds)
                     //System.out.println("intersect detected");
 
                     int winding;
@@ -168,41 +189,49 @@ public class ScubaDubaAI extends AI {
                         winding = 1;
                     }
 
+
                     double xVec = target.getX() - info.getX();
                     double yVec = target.getY() - info.getY();
 
+                    // rotate forward vector by 1 degree depending on winding order
                     double newX = xVec * Math.cos(winding * 0.174) - yVec * Math.sin(winding * 0.174);
                     double newY = xVec * Math.sin(winding * 0.174) + yVec * Math.cos(winding * 0.174);
 
+                    // if stuck rotate forward vector 180 degrees (probably not needed anymore)
                     if(stuck) {
                         newX = xVec * Math.cos(winding * Math.PI) - yVec * Math.sin(winding * Math.PI);
                         newY = xVec * Math.sin(winding * Math.PI) + yVec * Math.cos(winding * Math.PI);
                     }
 
+                    // create new point for target
                     Point2D newTarget = new Point2D.Double(newX + info.getX(), newY + info.getY());
 
-                    return avoidObstacles(newTarget, clockwise);
+                    return avoidObstacles(newTarget, clockwise); // recursion (check new target for obstacles)
                 }
 
 
-                lastPoint.setLocation((int) coordinates[0], (int) coordinates[1]);
+                lastPoint.setLocation((int) coordinates[0], (int) coordinates[1]); // remember last segment (needed for creating line)
             }
         }
 
-        if(nextPearl.getX() > 1320 && Math.abs(playerPos.getX() - nextPearl.getX()) > 10 && !virtualPearlUsed) {
+        if(nextPearl.getX() > 1320 && Math.abs(playerPos.getX() - nextPearl.getX()) > 10 && !virtualPearlUsed && playerPos.getY()<nextPearl.getY()-50) { // special cases (after dealing with them for 10 hours straight, this at least fits for the current seed)
             target = new Point2D.Double(nextPearl.getX(), info.getY());
         }
 
-        return target;
+        return target; // return new, obstacle-free target point
     }
 
+    /**
+     *
+     * @return returns next pearl to aim at. usually left to right, proximity has priority
+     */
     private Point getNextPearl() {
 
         Point nextPearl = pearls.get(0); // random pearl for start reference
 
         if(pearls.size()==1) return nextPearl;
         for(Point pearl : pearls) {
-            if(pearl.distance(new Point2D.Double(info.getX(), info.getY())) < 60) return pearl;
+            if(pearl.distance(new Point2D.Double(info.getX(), info.getY())) < 80) return pearl;
 
             if (pearl.x < nextPearl.x) {
                 nextPearl = pearl;
